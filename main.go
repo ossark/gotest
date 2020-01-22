51678e4558a1ccc105602081073d5a00d7aaf2a2
Oscar <oscar@kene.nu>
2020-01-22 21:53:52 +0100

    Added expiry

--- main.go
+++ main.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"net/http"
 	"io/ioutil"
 )
 
-var data = make(map[string]string)
+type item struct {
+    value  string
+    expiry time.Time
+}
+
+var data = make(map[string]*item)
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
 	switch r.Method {
     case "GET":
-		if data[r.URL.Path] != "" { 
-			fmt.Fprintf(w, data[r.URL.Path]) 
+		if data[r.URL.Path] != nil { 
+			fmt.Fprintf(w, data[r.URL.Path].value+"\n")
 		} else { 
 			w.WriteHeader(http.StatusNotFound) 
-	} 
+		} 
     case "POST":
     	body, err := ioutil.ReadAll(r.Body)
     	if err == nil { 
-    		data[r.URL.Path] = string(body) 
+    	 	data[r.URL.Path] = &item { value: string(body), expiry : time.Now().Add(time.Second * 1800) }
     	}
     default:
         fmt.Fprintf(w, "GET or POST please.")
     }
 }
 
+func clean() {
+	for {
+		time.Sleep(60 * time.Second)
+		for key, value := range data {
+			if time.Now().After(value.expiry) {
+				delete(data, key)
+			}
+		} 
+	}
+}
+
 func main() {
+	go clean()
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":1337", nil)
 }
